32459cf555c396b43685ad04f42f9540c9aae468
eedo_y <doup2001@naver.com>
2025-01-16 21:53:13 +0900

    ✨ feat : Kafka 관련 이벤트 처리 기능 구현

---
+++ consumer/src/main/java/com/fc/event/LikeEvent.java
@@ -0,0 +1,15 @@
+package com.fc.event;
+
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+public class LikeEvent {
+
+    private LikeEventType type;
+    private Long postId;
+    private Long userId;
+    private Instant createdAt;
+
+}
